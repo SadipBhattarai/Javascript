4b4a3d072dd15e84716e16c885feb06665c38137
SadipBhattarai <sadipbhattarai3@gmail.com>
2025-11-18 18:52:42 +0545

    Learned Unary Operator

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
-console.log("Hello");
+// Write a program that asks user for their name and alerts their name
+// With greeting
 
-const susan = "boy";
-console.log(susan);
+/*
+const username = prompt("What is your name?");
+alert(`Welcome, ${username}`);
+*/
 
-let man = "susan";
-console.log(man);
+// const susanSalary ="50000";
+// console.log(typeof susanSalary);
+// actualSusanSalary = Number(susanSalary);
+// console.log(susanSalary);
+// console.log(typeof actualSusanSalary, typeof susanSalary);
+// console.log(actualSusanSalary, susanSalary);
 
-var boy = "Sangit";
-console.log(boy);
+// Write a program that divides the two numbers asking user for the 2 number inputs and alert the result.
+
+// const number1 = +prompt ("Enter the First Number");
+// const number2 = +prompt ("Enter the Second Number");
+// const result= number1/number2;
+// alert(`The result of division between ${number1} and ${number2} is ${result}`);
+
+// Unary Operator
+
+let val = 1;
+val+=10;
+console.log(val);
